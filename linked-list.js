e1db4d4e6f4eea6cb4d5ce4610b81964e5cd9e88
Steerpike <imthekoul@gmail.com>
2022-08-07 14:59:48 +0300

    Delete && deleteAt methods

--- linked-list.js
+++ linked-list.js
@@ -118,6 +118,39 @@ class LinkedList {
       }
     }
   }
+  delete(x) {
+    if (x !== this.head.datum) {
+      let prev = this.head;
+      let temp = this.head;
+      while (temp !== null) {
+        if (temp.datum === x) {
+          prev.next = temp.next;
+          return temp;
+        }
+        prev = temp;
+        temp = temp.next;
+      }
+      return null;
+    } else {
+      let temp = this.head;
+      this.head = this.head.next;
+      return temp;
+    }
+  }
+  removeAt(index) {
+    if (index < 0) return console.log('Enter a valid index');
+    if (index > this.size()) return console.log('Index greater than list-size');
+    else if (index === 0) this.delete(this.head.datum);
+    else {
+      let i = 0;
+      let temp = this.head;
+      while (i < index - 1) {
+        temp = temp.next;
+        i++;
+      }
+      this.delete(temp.next.datum);
+    }
+  }
 }
 
 let testList = new LinkedList();
@@ -135,4 +168,10 @@ testList.prepend(5);
 testList.toString();
 
 testList.insertAt(113, 0);
+testList.toString();
+
+testList.prepend(42);
+testList.toString();
+
+testList.removeAt(0);
 testList.toString();
