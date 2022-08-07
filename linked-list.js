d8b7bc51f05c8ce829d5ba3b18dff841625323dd
Steerpike <imthekoul@gmail.com>
2022-08-07 13:38:10 +0300

    size tails atIndex contains && find methods added

--- linked-list.js
+++ linked-list.js
@@ -24,4 +24,58 @@ class LinkedList {
     this.head = new Node(x);
     this.head.next = temp;
   }
+  size() {
+    let size = 0;
+    if (head === null) return size;
+    else {
+      let temp = this.head;
+      while (temp.next !== null) {
+        temp = temp.next;
+        ++size;
+      }
+      return size;
+    }
+  }
+  getHead() {
+    return this.head;
+  }
+  getTail() {
+    if (this.head === null) return this.head;
+    else {
+      let temp = this.head;
+      while (temp.next !== null) {
+        temp = temp.next;
+      }
+      return temp;
+    }
+  }
+  find(x) {
+    if (this.head !== null) {
+      let temp = this.head;
+      while (temp !== null) {
+        if (temp.datum === x) return temp;
+        temp = temp.next;
+      }
+    } else return null;
+  }
+  at(index) {
+    if (this.head === null) return null;
+    if (index === 1) return this.head;
+    let temp = this.head;
+    let count = 1;
+    while (count <= index) {
+      ++count;
+      if (temp.next === null) return 'Not found';
+      temp = temp.next;
+    }
+    return temp;
+  }
+  contains(x) {
+    let temp = this.head;
+    while (temp.next !== null) {
+      if (temp.datum === x) return true;
+      temp = temp.next;
+    }
+    return false;
+  }
 }
